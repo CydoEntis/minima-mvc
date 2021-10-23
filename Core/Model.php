9d910c7944e5314a5f6d68c985a82d0ffa75ba6e
Cody Stine <cydoentis@gmail.com>
2021-10-23 14:11:05 -0400

    Added a configuration file for Database connections

--- Core/Model.php
+++ Core/Model.php
@@ -4,6 +4,7 @@ namespace Core;
 
 use PDO;
 use PDOException;
+use App\Config;
 
 abstract class Model
 {
@@ -13,13 +14,9 @@ abstract class Model
     static $db = null;
 
     if ($db === null) {
-      $host = "localhost";
-      $dbname = "mvc";
-      $username = "root";
-      $password = "";
-
       try {
-        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password);
+        $dsn = "mysql:host=" . Config::DB_HOST . ";dbname=" . Config::DB_NAME . ";charset=utf8";
+        $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
       } catch (PDOException $e) {
         echo $e->getMessage();
       }
